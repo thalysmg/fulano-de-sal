a196ec2d1afb760e6e8d797931b8edd6500ce136
thaisnat <thais.toscano@ccc.ufcg.edu.br>
2019-07-11 14:14:36 -0300

    dados pessoais e menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RouterModule } from '@angular/router';
 
@@ -8,12 +9,19 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginAdminComponent } from './login-admin/login-admin.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { DadosPessoaisComponent } from './dados-pessoais/dados-pessoais.component';
+import { ItensMarmitaComponent } from './itens-marmita/itens-marmita.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginAdminComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    DadosPessoaisComponent,
+    ItensMarmitaComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +35,18 @@ import { HomeComponent } from './home/home.component';
       {
         path: 'login',
         component: LoginAdminComponent
+      },
+      {
+        path: 'attDados',
+        component: DadosPessoaisComponent
+      },
+      {
+        path: 'menu',
+        component: MenuComponent
+      },
+      {
+        path: 'itens',
+        component: ItensMarmitaComponent
       }
     ])
   ],
---
+++ src/app/itens-marmita/itens-marmita.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itens-marmita',
+  templateUrl: './itens-marmita.component.html',
+  styleUrls: ['./itens-marmita.component.css']
+})
+export class ItensMarmitaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
