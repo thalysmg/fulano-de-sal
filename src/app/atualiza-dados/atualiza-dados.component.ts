6f87bbd6e86c4b59784955da7037cd1f5fbc1770
Thalys Menezes <thalys.gadelha@ccc.ufcg.edu.br>
2019-07-11 15:49:46 -0300

    Tela 'atualiza-dados criada'

---
+++ src/app/atualiza-dados/atualiza-dados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atualiza-dados',
+  templateUrl: './atualiza-dados.component.html',
+  styleUrls: ['./atualiza-dados.component.css']
+})
+export class AtualizaDadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
