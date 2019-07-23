b7f00eb5626f1d40a8c9b52c989a87d684f11c2e
Thalys Menezes <thalys.gadelha@ccc.ufcg.edu.br>
2019-07-23 18:56:39 -0300

    Tela de Login funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators
-} from '../../node_modules/@angular/forms';
-import { DadosPessoaisComponent } from './dados-pessoais/dados-pessoais.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  
-  formularioDeUsuario: FormGroup;
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.criarFormularioDeUsuario();
-  }
-
-  enviarDados(){
-    const dadosFormulario = this.formularioDeUsuario.value;
-    
-    const usuario = new DadosPessoaisComponent(
-      dadosFormulario.nomeUsuario,
-      dadosFormulario.telefone
-    );
-
-    alert(`O usuário ${usuario.nomeUsuario} foi cadastrado com sucesso. \n Dados: ${JSON.stringify(usuario)}`);
-  
-    this.formularioDeUsuario.reset();
-  }
-
-  criarFormularioDeUsuario() {
-    this.formularioDeUsuario = this.fb.group({
-      nomeUsuario: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(100)
-        ])
-      ]
-      });
-  }
-
-  get nomeUsuario() {
-    return this.formularioDeUsuario.get('nomeUsuario');
-  }
-
-  get telefone(){
-    return this.formularioDeUsuario.get('telefone');
-  }
+export class AppComponent {
+  title = 'fulano-de-sal';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule, Component } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginAdminComponent } from './login-admin/login-admin.component';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { DadosPessoaisComponent } from './dados-pessoais/dados-pessoais.component';
-import { ItensMarmitaComponent } from './itens-marmita/itens-marmita.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatGridListModule, MatButtonModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginAdminComponent,
-    HomeComponent,
-    MenuComponent,
-    DadosPessoaisComponent,
-    ItensMarmitaComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    // Adicionando o módulo de formulários reativos no módulo da nossa aplicação
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        component: HomeComponent
-      },
-      {
-        path: 'login',
-        component: LoginAdminComponent
-      },
-      {
-        path: 'dadosPessoais',
-        component: DadosPessoaisComponent
-      },
-      {
-        path: 'menu',
-        component: MenuComponent
-      },
-      {
-        path: 'itens',
-        component: ItensMarmitaComponent
-      }
-    ])
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
-  providers: [ {provide: String, useValue: "stringValue"}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
