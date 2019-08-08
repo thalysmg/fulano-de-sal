7476e6618dd5df6238ce2b4ddb448d1f5feea7dd
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2019-08-08 15:43:08 -0300

    Adiciona funcao que verifica se o item adicionado tem preco, se nao tiver, atualiza

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,9 @@ const firestore = admin.firestore();
 //  response.send("Hello from Firebase!");
 // });
 
+/*
+    Funcao chamada no momento em que um novo usuario faz o registro no webapp
+*/
 exports.registerUser = functions.auth.user().onCreate((user, context) => {
     userData = {
         email: user.email,
@@ -27,6 +30,7 @@ exports.registerUser = functions.auth.user().onCreate((user, context) => {
     })
 })
 
+/* Adiciona algumas informações relevantes (timestamp e disponibilidade) ao criar um novo cardapio */
 exports.createMenu = functions.firestore.document('menu/{menuId}').onCreate((snap, context) => {
     const menuValue = snap.data();
     menuValue.timestamp = Date.now();
@@ -41,6 +45,36 @@ exports.createMenu = functions.firestore.document('menu/{menuId}').onCreate((sna
         console.log('Erro ao atualizar timestamp e disponibilidade do menu');
         console.log(err);
     })
+})
+
+/* 
+    Funcao chamada ao criar um novo item na seçao de bebidas/sobremesas:
+    Se o item não tiver preco, é adicionado um preço padrão de 0.00
+*/
+exports.createItem = functions.firestore.document('sections/{sectionId}').onUpdate((snap, context) => {
+    if(context.params.sectionId === 'Bebidas' || context.params.sectionId === 'Sobremesas'){
+        var currentItems = snap.after.data().opcoes;
+        notAllHavePrice = currentItems.some((item) => {return typeof item.unitPrice === "undefined";}) //Indica se todos os items tem a prop. 'unitPrice'
+        if(notAllHavePrice){
+            currentItems.forEach((item) => {
+                if((typeof item.unitPrice) === "undefined"){
+                    item.unitPrice = 0.00;
+                    console.log('Item antes sem preço teve seu preço atualizado.');
+                }
+            })
+            firestore.collection('sections').doc(context.params.sectionId).set({
+                opcoes: currentItems
+            }, {merge: true})
+            .then(res => {
+                console.log('Itens antes sem preço atualizados com sucesso');
+                return res;                
+            })
+            .catch(err => {
+                console.log('Erro ao atualizar itens sem preços: ');
+                console.log(err);
+            })         
+        }
+    }
 })
 
 
