3f08c275ba4a37a8ec1dd654e8329dc511d0078c
Lílian <lilianhteixeira@gmail.com>
2019-08-10 17:36:32 -0300

    Criação da tela do usuário para atualizar o cadastro ou pedir marmita.

---
+++ src/app/tela-usuario/tela-usuario.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TelaUsuarioComponent } from './tela-usuario.component';
+
+describe('TelaUsuarioComponent', () => {
+  let component: TelaUsuarioComponent;
+  let fixture: ComponentFixture<TelaUsuarioComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TelaUsuarioComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TelaUsuarioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/tela-usuario/tela-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-usuario',
+  templateUrl: './tela-usuario.component.html',
+  styleUrls: ['./tela-usuario.component.css']
+})
+export class TelaUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
